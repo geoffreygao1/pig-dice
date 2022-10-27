98f125bff991870de43e966aa372a0a18dcc9daa
Geoffrey Gao <geoffreygao1@gmail.com>
2022-10-26 20:03:11 -0700

    Changed alert to display math equations

--- js/scripts.js
+++ js/scripts.js
@@ -24,9 +24,16 @@ const number2 = parseInt(prompt("Enter another number:"));
 // window.alert("Your multiplication result is " + multiply(number1, number2));
 // window.alert("Your division result is " + divide(number1, number2));
 
-const addMsg = "Your addition result is " + add(number1, number2) + ".";
-const subtractMsg = "Your subtraction result is " + subtract(number1, number2) + ".";
-const multiplyMsg = "Your multiplication result is " + multiply(number1, number2) + ".";
-const divideMsg = "Your multiplication result is " + divide(number1, number2) + ".";
+// const addMsg = "Your addition result is " + add(number1, number2) + ".";
+// const subtractMsg = "Your subtraction result is " + subtract(number1, number2) + ".";
+// const multiplyMsg = "Your multiplication result is " + multiply(number1, number2) + ".";
+// const divideMsg = "Your multiplication result is " + divide(number1, number2) + ".";
 
-window.alert(addMsg + subtractMsg + multiplyMsg + divideMsg);
+// window.alert(addMsg + subtractMsg + multiplyMsg + divideMsg);
+
+const addMsg = number1 + " + " + number2 + " = " + add(number1, number2);
+const subMsg = number1 + " - " + number2 + " = " + subtract(number1, number2);
+const multMsg = number1 + " * " + number2 + " = " + multiply(number1, number2);
+const divMsg = number1 + " / " + number2 + " = " + divide(number1, number2);
+
+window.alert(addMsg + ", " + subMsg + ", " + multMsg + ", " + divMsg);
