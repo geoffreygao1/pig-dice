4251d70215213ffd86fc7c49d7eefe555621a95c
Geoffrey Gao <geoffreygao1@gmail.com>
2022-11-17 11:48:36 -0800

    Added numConvert function

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,16 @@ function numSplit(number) {
   });
   return numSplitArray;
 }
+
+function numConvert(number) {
+  let numSplitArray = numSplit(number);
+  if (numSplitArray.includes(3)) {
+    return "Won't you be my neighbor?";
+  } else if (numSplitArray.includes(2)) {
+    return "Boop!";
+  } else if (numSplitArray.includes(1)) {
+    return "Beep!";
+  } else {
+    return number;
+  }
+}
