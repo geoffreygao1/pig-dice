77eb2dbb3b3f6176ce95d6c7df458ebcc438d48e
Geoffrey Gao <geoffreygao1@gmail.com>
2022-11-27 14:27:55 -0800

    added remove and reopen functionalitY

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 // Business Logic for Tasks
-function Task(name, description, priority, dueDate, isDone) {
+function Task(name, description, priority, dueDate) {
   this.name = name;
   this.description = description;
   this.priority = priority.toLowerCase();
   this.dueDate = new Date(dueDate); // date format : mm/dd/yyyy
-  this.isDone = isDone;
+  this.isDone = false;
 }
 
 Task.prototype.toString = function () {
-  return this.name + " " + this.isDone;
+  return `${this.name}: ${this.description}, ${this.dueDate.toLocaleDateString()}`;
 }
 
 
@@ -23,7 +23,6 @@ ToDoList.prototype.addTask = function (task) {
 
 ToDoList.prototype.removeTask = function (index) {
   this.tasks.splice(index, 1);
-  // Add functionality to return removed task
 }
 
 ToDoList.prototype.finishTask = function (index) {
@@ -32,6 +31,10 @@ ToDoList.prototype.finishTask = function (index) {
 
 ToDoList.prototype.reOpen = function (index) {
   this.tasks[index].isDone = false;
+}
+
+ToDoList.prototype.toggle = function (index) {
+  this.tasks[index].isDone = !this.tasks[index].isDone;
 }
 
 ToDoList.prototype.sortTasks = function (by) {
@@ -49,5 +52,78 @@ ToDoList.prototype.sortTasks = function (by) {
     } else {
       return 1;
     }
+  });
+}
+
+
+function buildList(list) {
+  const incompleteOl = document.createElement("ol");
+  const completeOl = document.createElement("ol");
+  list.tasks.forEach(function (element, index) {
+    let li = document.createElement("li");
+    li.append(element.toString());
+
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    removeButton.id = index;
+    removeButton.addEventListener("click", function () {
+      removeTaskHandler(list, index)
+    });
+    li.append(removeButton);
+
+    let completeButton = document.createElement("button");
+    completeButton.id = index;
+    completeButton.addEventListener("click", function () {
+      toggleTaskHandler(list, index)
+    });
+    li.append(completeButton);
+    if (element.isDone) {
+      completeButton.innerText = "Reopen";
+      completeOl.append(li);
+    } else {
+      completeButton.innerText = "Complete";
+      incompleteOl.append(li);
+    }
+    ;
+  })
+  return [incompleteOl, completeOl]
+}
+
+function updateList(list) {
+  const listArea = document.getElementById("toDoList");
+  const completeArea = document.getElementById("completedTasks");
+  listArea.innerHTML = "";
+  completeArea.innerHTML = "";
+  let [incompleteOl, completeOl] = buildList(list);
+  listArea.append(incompleteOl);
+  completeArea.append(completeOl);
+}
+
+function addTaskHandler(list) {
+  event.preventDefault();
+  const name = document.getElementById("inputName").value;
+  const description = document.getElementById("inputDescription").value;
+  const priorities = document.getElementById("priorities").value;
+  const inputDate = document.getElementById("inputDate").value;
+  const newTask = new Task(name, description, priorities, inputDate);
+  list.addTask(newTask);
+  updateList(list);
+}
+
+function removeTaskHandler(list, index) {
+  list.removeTask(parseInt(index));
+  updateList(list);
+}
+
+function toggleTaskHandler(list, index) {
+  list.toggle(index);
+  updateList(list);
+}
+
+window.onload = function () {
+  const toDoList = new ToDoList();
+  const addForm = document.getElementById("taskInput");
+  addForm.addEventListener("submit", function () {
+    addTaskHandler(toDoList);
   });
 }
