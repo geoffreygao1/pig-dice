34a5558a9429dd9d78f8f8c02beeb5a6c3e4d859
Geoffrey Gao <geoffreygao1@gmail.com>
2022-11-27 09:52:54 -0800

    Initialized HTML, CSS, Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,112 +1,42 @@
-//Business Logic
+//Business logic for addressbook
+function AddressBook() {
+  this.contacts = {};
+  this.currentId = 0;
+}
 
-//Returns an array with index values of 0 up to an input number
-function numCounter(number) {
-  let numArray = [];
-  for (let i = 0; i <= number; i++) {
-    numArray.push(i);
+AddressBook.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+AddressBook.prototype.addContact = function (contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
+};
+
+AddressBook.prototype.findContact = function (id) {
+  if (this.contacts[id] !== undefined) {
+    return this.contacts[id];
   }
-  return numArray;
-}
+  return false;
+};
 
-//Returns an array with index values of each digit of an input number
-function numSplit(number) {
-  const numStringArray = number.toString().split('')
-  const numSplitArray = numStringArray.map(function (element) {
-    return parseInt(element);
-  });
-  return numSplitArray;
-}
-
-//Returns a string or number based on whether a digit exists in an input number
-function numConvert(number, name) {
-  let numSplitArray = numSplit(number);
-  if (numSplitArray.includes(3)) {
-    return "Won't you be my neighbor, " + name + "?";
-  } else if (numSplitArray.includes(2)) {
-    return "Boop!";
-  } else if (numSplitArray.includes(1)) {
-    return "Beep!";
-  } else {
-    return number;
-  }
-}
-
-//Returns an array of converted numbers counting from 0 to the input number
-function roboger(number, name) {
-  let counter = numCounter(number);
-  let outputArray = counter.map(function (element) {
-    return numConvert(element, name);
-  })
-  return outputArray;
-}
-
-//Returns an array of converted numbers counting from the input number to 0
-function reverseRoboger(number, name) {
-  let counter = numCounter(number).reverse();
-  let outputArray = counter.map(function (element) {
-    return numConvert(element, name);
-  })
-  return outputArray;
-}
-
-//UI Logic
-function checkForms() {
-  const num = document.getElementById("inputNum").value;
-  const name = document.getElementById("inputName").value;
-  if (num && name) {
-    return true;
-  } else {
+AddressBook.prototype.deleteContact = function (id) {
+  if (this.contacts[id] === undefined) {
     return false;
   }
-}
+  delete this.contacts[id];
+  return true;
+};
 
-function generateHandler() {
-  let outputDiv = document.getElementById("output");
-  outputDiv.innerHTML = '';
-  if (checkForms()) {
-    let h2 = document.createElement('h3');
-    h2.innerText = "Mr. Roboger Says:";
-    outputDiv.append(h2);
-    const num = document.getElementById("inputNum").value;
-    const name = document.getElementById("inputName").value;
-    let outputArray = roboger(num, name);
-    outputArray.forEach(function (element) {
-      let itemDiv = document.createElement('p');
-      itemDiv.innerText = "> " + element;
-      outputDiv.append(itemDiv);
-    });
-  } else {
-    outputDiv.innerText = "Please make sure to fill in all fields"
-  }
-}
-
-function reverseHandler() {
-  let outputDiv = document.getElementById("output");
-  outputDiv.innerHTML = '';
-  if (checkForms()) {
-    const num = document.getElementById("inputNum").value;
-    const name = document.getElementById("inputName").value;
-    let h2 = document.createElement('h3');
-    h2.innerText = "Mr. Roboger Says:";
-    outputDiv.append(h2);
-    let outputArray = reverseRoboger(num, name);
-    outputArray.forEach(function (element) {
-      let itemDiv = document.createElement('p');
-      itemDiv.innerText = "> " + element;
-      outputDiv.append(itemDiv);
-    });
-  } else {
-    outputDiv.innerText = "Please make sure to fill in all fields"
-  }
+//business logic for contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = FirstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
 }
 
 
-window.addEventListener("load", function () {
-  const form = document.getElementById("input");
-  const generate = document.getElementById("generate");
-  const reverse = document.getElementById("reverse");
-
-  generate.addEventListener("click", generateHandler);
-  reverse.addEventListener("click", reverseHandler);
-})
+Contact.prototype.fullName = function () {
+  return this.firstName + " " + this.lastName;
+};
