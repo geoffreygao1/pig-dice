81a486b9a9a55accd4a076a80b7ca7d9763d0c44
Geoffrey Gao <geoffreygao1@gmail.com>
2022-11-27 11:30:47 -0800

    added sort functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,13 @@
 function Task(name, description, priority, dueDate, isDone) {
   this.name = name;
   this.description = description;
-  this.priority = priority;
-  this.dueDate = dueDate;
+  this.priority = priority.toLowerCase();
+  this.dueDate = new Date(dueDate); // date format : mm/dd/yyyy
   this.isDone = isDone;
+}
+
+Task.prototype.toString = function () {
+  return this.name + " " + this.isDone;
 }
 
 
@@ -24,4 +28,26 @@ ToDoList.prototype.removeTask = function (index) {
 
 ToDoList.prototype.finishTask = function (index) {
   this.tasks[index].isDone = true;
+}
+
+ToDoList.prototype.reOpen = function (index) {
+  this.tasks[index].isDone = false;
+}
+
+ToDoList.prototype.sortTasks = function (by) {
+  this.tasks.sort(function (a, b) {
+    if (a.priority === b.priority) {
+      if (a[by] < b[by]) {
+        return -1;
+      } else if (b[by] < a[by]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    } else if (a.priority === "high") {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
 }
